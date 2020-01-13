ecb131f76dbfec1aeb828a96b9c873da45bad3ad
kyungah <govl19@forbiz.co.kr>
2020-01-13 18:14:59 +0900

    중간저장

---
+++ chapter9/answer/answer_kyungah/src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<style>
+    * {  box-sizing: border-box;  }
+    .header {
+        background-color: purple; padding: 30px 30px;
+        color: yellow; text-align: center;
+    }
+    .header:after {
+        content: ""; display: table; clear: both;
+    }
+</style>
+<template>
+    <div id="todolistapp">
+        <div id="header" class="header">
+            <h2>TO DO LIST</h2>
+            <todo-input />
+        </div>
+        <list></list>
+    </div>  
+</template>
+<script type="text/javascript">
+import todoInput from './todoInput';
+import List from './List';
+export default {
+    name : 'todo-list',
+    components : { todoInput, List }
+}
+</script>
