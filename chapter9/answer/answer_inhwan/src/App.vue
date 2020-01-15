62dc9585b9f3c8ff8915a2dc863a7b06a60e7d7e
BaeIn-hwan <ih9137@naver.com>
2020-01-16 01:22:56 +0900

    todolist

---
+++ chapter9/answer/answer_inhwan/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app">
+		<todo></todo>
+	</div>
+</template>
+<script>
+import todo from './components/todo.vue';
+
+export default {
+  components : { todo },
+}
+</script>
+
+<style scoped>
+	* {
+		margin: 0;
+		padding: 0;
+	}
+
+	ul {
+		list-style: none;
+	}
+</style>
