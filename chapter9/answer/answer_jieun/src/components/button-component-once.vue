63f108f77548172d29a42b9e353b92e81b43dc66
joannashin <joannashin3@gmail.com>
2020-01-16 00:35:15 +0900

    answer for chapter9

---
+++ chapter9/answer/answer_jieun/src/components/button-component-once.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h3>한번만찍기</h3>
+        <span :currentView="current">{{current}}</span>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'oncebutton',
+        data() {
+            return {
+                current: "2020.01.16"
+            }
+        }
+    }
+</script>
