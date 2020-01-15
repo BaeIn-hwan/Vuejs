958d301942e98c48193fb08828b76090447a9b27
j02517 <57432574+j02517@users.noreply.github.com>
2020-01-16 08:40:15 +0900

    chapter9

---
+++ chapter9/answer/answer_younghee/src/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+</template>
+<script>
+    import HelloWord from './components/HelloWords.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld
+        }
+    }
+</script>
--- chapter9/answer/answer_younghee/src/components/List.vue
+++ chapter9/answer/answer_younghee/src/components/List.vue
@@ -44,10 +44,10 @@ export default {
     data : function() {
         return {
             todolist : [
-                { id:1, todo : "영화보기", done:false },
-                { id:2, todo : "주말 산책", done:true },
-                { id:3, todo : "ES6 학습", done:false },
-                { id:4, todo : "잠실 야구장", done:false },
+                { id:1, todo : "청소하기", done:false },
+                { id:2, todo : "영화보기", done:true },
+                { id:3, todo : "설날 내려갈 준비하기", done:false },
+                { id:4, todo : "친구만나기", done:false },
             ]
         }
     },
